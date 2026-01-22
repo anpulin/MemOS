78078f4bb6b772ebe3cc2f9722d98b53be677390
安赋生 <anfusheng@anfushengdeMacBook-Pro.local>
2026-01-22 15:25:04 +0800

    添加日志

--- src/memos/settings.py
+++ src/memos/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 MEMOS_DIR = Path(os.getenv("MEMOS_BASE_PATH", Path.cwd())) / ".memos"
-DEBUG = False
+DEBUG = True
 
 # "memos" or "memos.submodules" ... to filter logs from specific packages
 LOG_FILTER_TREE_PREFIX = ""
